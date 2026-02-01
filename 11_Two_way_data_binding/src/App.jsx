72fb724352deaecbace3ffed86e8d7ce6d711a52
Suman <suman@localhost.localdomain>
2026-02-02 00:36:46 +0530

    Added 11_Two_way_data_binding folder

---
+++ 11_Two_way_data_binding/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const App = () => {
+
+  const [name, setName] = useState('')
+
+  function formsubmit(e){
+    e.preventDefault()
+    console.log('Form submitedddddd by', name);
+    setName('')
+    
+  }
+
+
+
+
+  return (
+    <div>
+      <form onSubmit={(e)=>{
+        formsubmit(e)
+      }}>
+        <input 
+        type="text" 
+        placeholder='Enter your name'
+        value={name}
+        onChange={(e)=>{
+        setName(e.target.value);
+        }}
+        />
+        <button>Submit</button>
+      </form>
+      
+    </div>
+  )
+}
+
+export default App
