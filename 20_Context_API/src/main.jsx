d97fd4cb885817843f911df92006f9b535813f7a
Suman Maity <someone.cpp@proton.me>
2026-03-05 01:15:39 +0530

    Update main.jsx

--- 20_Context_API/src/main.jsx
+++ 20_Context_API/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,6 +6,5 @@ import ThemeContext from './context/ThemeContext.jsx'
 createRoot(document.getElementById('root')).render(
   <ThemeContext>
     <App />
-  </ThemeContext>,
-  
+  </ThemeContext>
 )
